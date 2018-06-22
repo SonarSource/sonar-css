08d6560282dcd46710d7ad2db4ab8a4bb969e116
Amaury Levé <amaury.leve@sonarsource.com>
2018-06-22 14:41:52 +0200

    Rule S4655: '!important' should not be used on 'keyframes' (#61)

--- sonar-css-plugin/src/main/java/org/sonar/css/plugin/CssRules.java
+++ sonar-css-plugin/src/main/java/org/sonar/css/plugin/CssRules.java
@@ -24,6 +24,7 @@ import org.sonar.api.batch.rule.Checks;
 import org.sonar.api.rule.RuleKey;
 import org.sonar.css.plugin.rules.CommentNoEmpty;
 import org.sonar.css.plugin.rules.CssRule;
+import org.sonar.css.plugin.rules.KeyframeDeclarationNoImportant;
 import org.sonar.css.plugin.rules.NoEmptySource;
 import org.sonar.css.plugin.rules.ColorNoInvalidHex;
 import org.sonar.css.plugin.rules.StringNoNewline;
@@ -54,11 +55,12 @@ public class CssRules {
 
   public static List<Class> getRuleClasses() {
     return Collections.unmodifiableList(Arrays.asList(
-        ColorNoInvalidHex.class,
-        CommentNoEmpty.class,
-        NoEmptySource.class,
-        StringNoNewline.class,
-        UnitNoUnknown.class
+      ColorNoInvalidHex.class,
+      CommentNoEmpty.class,
+      KeyframeDeclarationNoImportant.class,
+      NoEmptySource.class,
+      StringNoNewline.class,
+      UnitNoUnknown.class
     ));
   }
 
