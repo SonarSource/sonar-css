4fc865cac084522cd37000a5abbf95acf00f7977
Elena Vilchik <elena.vilchik@sonarsource.com>
2018-06-26 15:11:24 +0200

    Not fail analysis when syntax error (#80)

--- sonar-css-plugin/src/test/java/org/sonar/css/plugin/CssRuleSensorTest.java
+++ sonar-css-plugin/src/test/java/org/sonar/css/plugin/CssRuleSensorTest.java
@@ -145,6 +145,30 @@ public class CssRuleSensorTest {
     assertThat(logTester.logs(LoggerLevel.WARN)).contains("No rules are activated in CSS Quality Profile");
   }
 
+  @Test
+  public void test_syntax_error() throws IOException {
+    SensorContextTester context = SensorContextTester.create(BASE_DIR);
+    context.fileSystem().setWorkDir(tmpDir.getRoot().toPath());
+    DefaultInputFile inputFile = createInputFile(context, "some css content\n on 2 lines", "dir/file.css");
+    TestLinterCommandProvider rulesExecution = new TestLinterCommandProvider().nodeScript("/executables/mockSyntaxError.js", inputFile.absolutePath());
+    CssRuleSensor sensor = new CssRuleSensor(new TestBundleHandler(), checkFactory, rulesExecution);
+    sensor.execute(context);
+
+    assertThat(logTester.logs(LoggerLevel.ERROR)).contains("Failed to parse " + inputFile.uri());
+  }
+
+  @Test
+  public void test_unknown_rule() throws IOException {
+    SensorContextTester context = SensorContextTester.create(BASE_DIR);
+    context.fileSystem().setWorkDir(tmpDir.getRoot().toPath());
+    DefaultInputFile inputFile = createInputFile(context, "some css content\n on 2 lines", "dir/file.css");
+    TestLinterCommandProvider rulesExecution = new TestLinterCommandProvider().nodeScript("/executables/mockUnknownRule.js", inputFile.absolutePath());
+    CssRuleSensor sensor = new CssRuleSensor(new TestBundleHandler(), checkFactory, rulesExecution);
+    sensor.execute(context);
+
+    assertThat(logTester.logs(LoggerLevel.ERROR)).contains("Unknown stylelint rule or rule not enabled: 'unknown-rule-key'");
+  }
+
   private static DefaultInputFile createInputFile(SensorContextTester sensorContext, String content, String relativePath) {
     DefaultInputFile inputFile = new TestInputFileBuilder("moduleKey", relativePath)
       .setModuleBaseDir(sensorContext.fileSystem().baseDirPath())
