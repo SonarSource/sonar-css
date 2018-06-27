09dec8f9284d49eec634ec2e9247beffaea172a6
Amaury Levé <amaury.leve@sonarsource.com>
2018-06-27 14:36:02 +0200

    Update S4662: Rule should be parameterized

--- sonar-css-plugin/src/main/java/org/sonar/css/plugin/rules/AtRuleNoUnknown.java
+++ sonar-css-plugin/src/main/java/org/sonar/css/plugin/rules/AtRuleNoUnknown.java
@@ -20,11 +20,19 @@
 package org.sonar.css.plugin.rules;
 
 import java.util.Arrays;
-import java.util.List;
 import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
 
 @Rule(key = "S4662")
 public class AtRuleNoUnknown implements CssRule {
+
+  private static final String DEFAULT_IGNORED_AT_RULES = "content,debug,each,else,for,function,if,include,mixin,return,while";
+
+  @RuleProperty(
+    key = "ignoreAtRules",
+    description = "Comma-separated list of \"at-rules\" to consider as valid.",
+    defaultValue = "" + DEFAULT_IGNORED_AT_RULES)
+  private String ignoredAtRules = DEFAULT_IGNORED_AT_RULES;
 
   @Override
   public String stylelintKey() {
@@ -36,7 +44,8 @@ public class AtRuleNoUnknown implements CssRule {
     return Arrays.asList(true, new StylelintIgnoreOption());
   }
 
-  private static class StylelintIgnoreOption {
-    private final List<String> ignoreAtRules = Arrays.asList("debug", "each", "else", "for", "function", "if", "include", "mixin", "return", "while");
+  private class StylelintIgnoreOption {
+    // Used by GSON serialization
+    private final String[] ignoreAtRules = ignoredAtRules.split(",");
   }
 }
