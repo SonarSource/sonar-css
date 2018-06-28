8789f5e8dad55a964a91974a7418abc600a8d15b
Elena <elena.vilchik@sonarsource.com>
2018-06-28 13:43:02 +0200

    Add 'at-root', 'error' and 'warn' to default of S4662

--- sonar-css-plugin/src/test/java/org/sonar/css/plugin/rules/CssRuleTest.java
+++ sonar-css-plugin/src/test/java/org/sonar/css/plugin/rules/CssRuleTest.java
@@ -53,7 +53,7 @@ public class CssRuleTest {
   @Test
   public void at_rule_unknown_default() {
     String optionsAsJson = new Gson().toJson(new AtRuleNoUnknown().stylelintOptions());
-    assertThat(optionsAsJson).isEqualTo("[true,{\"ignoreAtRules\":[\"content\",\"debug\",\"each\",\"else\",\"for\",\"function\",\"if\",\"include\",\"mixin\",\"return\",\"while\"]}]");
+    assertThat(optionsAsJson).isEqualTo("[true,{\"ignoreAtRules\":[\"at-root\",\"content\",\"debug\",\"each\",\"else\",\"error\",\"for\",\"function\",\"if\",\"include\",\"mixin\",\"return\",\"warn\",\"while\"]}]");
   }
 
   @Test
