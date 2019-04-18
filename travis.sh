b875469fe569a091f7fd13836cf3f4e0e3353b55
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-04-18 15:08:26 +0200

    Update CI build to use JDK 11

--- travis.sh
+++ travis.sh
@@ -4,12 +4,10 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v54 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v56 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
-. installJDK8
-. ~/.local/bin/installMaven35
 
 export DEPLOY_PULL_REQUEST=true
 
