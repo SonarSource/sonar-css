f4e53a38e2fb3fdc950fec6f40b0d52c9b18098d
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2020-10-01 14:46:06 +0200

    Update rules metadata

--- sonar-css-plugin/src/test/java/org/sonar/css/plugin/SonarWayProfileTest.java
+++ sonar-css-plugin/src/test/java/org/sonar/css/plugin/SonarWayProfileTest.java
@@ -38,7 +38,8 @@ public class SonarWayProfileTest {
     assertThat(profile.language()).isEqualTo(CssLanguage.KEY);
     assertThat(profile.name()).isEqualTo(SonarWayProfile.PROFILE_NAME);
     assertThat(profile.rules()).extracting("repoKey").containsOnly(CssRulesDefinition.REPOSITORY_KEY);
-    assertThat(profile.rules()).extracting("ruleKey").hasSize(CssRules.getRuleClasses().size() - 1);
+    // org.sonar.css.plugin.rules.FunctionCalcNoUnspacedOperator is not part of SonarWay
+    assertThat(profile.rules()).extracting("ruleKey").hasSize(CssRules.getRuleClasses().size() - 2);
   }
 
 }
