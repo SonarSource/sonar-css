116cdb49e8e97c0ecf8cc1639e12f321f2e48883
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2022-11-16 00:59:48 +0100

    Update Orchestrator to 3.40.0.183

--- its/ruling/src/test/java/org/sonar/css/its/CssRulingTest.java
+++ its/ruling/src/test/java/org/sonar/css/its/CssRulingTest.java
@@ -43,6 +43,7 @@ public class CssRulingTest {
 
   @ClassRule
   public static final Orchestrator ORCHESTRATOR = Orchestrator.builderEnv()
+    .useDefaultAdminCredentialsForBuilds(true)
     .setSonarVersion(System.getProperty(SQ_VERSION_PROPERTY, DEFAULT_SQ_VERSION))
     .addPlugin(MavenLocation.of("org.sonarsource.php", "sonar-php-plugin", "LATEST_RELEASE"))
     .addPlugin(MavenLocation.of("org.sonarsource.html", "sonar-html-plugin", "LATEST_RELEASE"))
